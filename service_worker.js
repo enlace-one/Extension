d00ca24433cf8fb918b20278a156ffdf5eaefd4f
ExplainItAgain <143844582+ExplainItAgain@users.noreply.github.com>
2024-02-26 13:33:01 -0600

    Smart Values for copy paste

--- service_worker.js
+++ service_worker.js
@@ -41,7 +41,7 @@ chrome.commands.onCommand.addListener(async (command) => {
             chrome.sidePanel.open({ tabId: tab.id });
           });
     } else if (command.startsWith("copy-value")) {
-        await addToClipboard(await eGet(command))
+        await addToClipboard(await convertSmartValues(await eGet(command)))
     } else if (command.startsWith("start-stop-clip-combiner")) {
 
     } else if (command === "open-extension-tab") {
@@ -74,3 +74,51 @@ chrome.commands.onCommand.addListener(async (command) => {
   async function addToClipboardV2(value) {
     navigator.clipboard.writeText(value);
   } 
+
+
+// Handle SmartValues like {{ month }}, {{ day }}, {{ date }}, {{ time }}, {{ year }}, {{ yy }}, {{ yyyy }}, {{ MM }}, {{ dd }}, {{ hh }}, {{ mm }}, {{ ss }}, {{ weekday }}
+// Does not matter if there is a space or not. Ex: {{month}} or {{ month }}
+async function convertSmartValues(string) {
+    var date = new Date()
+    var year = date.getFullYear()
+    var month = date.getMonth() + 1
+    var day = date.getDate()
+    var hour = date.getHours()
+    var minute = date.getMinutes()
+    var second = date.getSeconds()
+    var yy = year.toString().slice(2)
+    var MM = month.toString().padStart(2, '0')
+    var dd = day.toString().padStart(2, '0')
+    var hh = hour.toString().padStart(2, '0')
+    var mm = minute.toString().padStart(2, '0')
+    var ss = second.toString().padStart(2, '0')
+
+    string = string.replace(/{{\s*year\s*}}/g, year)
+    string = string.replace(/{{\s*day\s*}}/g, day)
+    string = string.replace(/{{\s*hour\s*}}/g, hour)
+    string = string.replace(/{{\s*minute\s*}}/g, minute)
+    string = string.replace(/{{\s*second\s*}}/g, second)
+    string = string.replace(/{{\s*yy\s*}}/g, yy)
+    string = string.replace(/{{\s*yyyy\s*}}/g, year)
+    string = string.replace(/{{\s*MM\s*}}/g, MM)
+    string = string.replace(/{{\s*dd\s*}}/g, dd)
+    string = string.replace(/{{\s*hh\s*}}/g, hh)
+    string = string.replace(/{{\s*mm\s*}}/g, mm)
+    string = string.replace(/{{\s*ss\s*}}/g, ss)
+
+    string = string.replace(/{{\s*date\s*}}/g, `${year}-${MM}-${dd}`)
+    string = string.replace(/{{\s*time\s*}}/g, `${hh}:${mm}:${ss}`)
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let dayName = days[date.getDay()];
+
+    string = string.replace(/{{\s*weekday\s*}}/g, dayName);
+
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let monthName = months[date.getMonth()];
+
+    string = string.replace(/{{\s*month\s*}}/g, monthName);
+
+    return string
+
+}
